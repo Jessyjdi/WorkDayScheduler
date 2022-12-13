daab7aa047ea6dcd663396e04dd47cb2a5001f79
Jessyjdi <116257968+Jessyjdi@users.noreply.github.com>
2022-12-12 23:50:55 -0800

    update with code

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,58 @@
+console.log(this);
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+$(document).ready(function () {
+  console.log("Ready");
+  //display current day & time on the top of the page below h1>p element
+  $("#currentDay").text(moment().format("dddd, MMMM Do")); // https://momentjs.com/docs/#/displaying/
+   $(".saveBtn").on("click", function () {
+      //get nearby values.
+      let text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+      let time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+
+  // Set item in local storage , Source: https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
+      localStorage.setItem(time, text);
+  })
+  //Get item from the local storage, 
+  $("#hour8 .description").val(localStorage.getItem("hour8"));
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour1 .description").val(localStorage.getItem("hour1"));
+  $("#hour2 .description").val(localStorage.getItem("hour2"));
+  $("#hour3 .description").val(localStorage.getItem("hour3"));
+  $("#hour4 .description").val(localStorage.getItem("hour4"));
+  $("#hour5 .description").val(localStorage.getItem("hour5"));
+
+  function dayScheduler() {
+      //get current number of hours.
+      let currentTime = moment().hour(); // https://day.js.org/en/
+      // [refrence sources: https://www.geeksforgeeks.org/moment-js-introduction/]
+         // loop over time blocks
+      $(".time-block").each(function () {
+          let blockTime = parseInt($(this).attr("id").split("-")[1]);
+          console.log( blockTime, currentTime)
+
+          if (blockTime < currentTime) {
+              $(this).addClass("past");
+              $(this).removeClass("future");
+              $(this).removeClass("present");
+          }
+          else if (blockTime === currentTime) {
+              $(this).removeClass("past");
+              $(this).addClass("present");
+              $(this).removeClass("future");
+          }
+          else {
+              $(this).removeClass("present");
+              $(this).removeClass("past");
+              $(this).addClass("future");
+          }
+      })
+  }
+  dayScheduler(); //call the function
+  console.log("End of the Day!");
+})
